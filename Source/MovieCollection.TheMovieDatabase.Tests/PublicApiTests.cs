4bf95f0d570e9d791bddf2623e1961cc6a821334
Peyman M. <16763358+peymanr34@users.noreply.github.com>
2024-03-06 01:26:53 +0330

    Add public api tests.

---
+++ Source/MovieCollection.TheMovieDatabase.Tests/PublicApiTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace MovieCollection.TheMovieDatabase.Tests
+{
+    public class PublicApiTests
+    {
+        [Fact]
+        public Task PublicApiShouldNotChange()
+        {
+            var publicApi = typeof(TheMovieDatabaseService).Assembly
+                .GeneratePublicApi();
+
+            return Verifier.Verify(publicApi);
+        }
+    }
+}
