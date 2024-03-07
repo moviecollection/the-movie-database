6b3f09d7cb6111f76dd045a44587c38de3254a6a
Peyman M. <16763358+peymanr34@users.noreply.github.com>
2024-03-07 23:40:43 +0330

    Make the response properties nullable.

--- Source/MovieCollection.TheMovieDatabase/Models/Base/Response.cs
+++ Source/MovieCollection.TheMovieDatabase/Models/Base/Response.cs
@@ -5,10 +5,13 @@
     public class Response
     {
         [JsonProperty("success")]
-        public bool Success { get; set; }
+        public bool? Success { get; set; }
 
+        /// <summary>
+        /// Gets or sets the tmdb status code (not an http status code).
+        /// </summary>
         [JsonProperty("status_code")]
-        public int StatusCode { get; set; }
+        public int? StatusCode { get; set; }
 
         [JsonProperty("status_message")]
         public string StatusMessage { get; set; }
